df05d8aba03905cd9dc6f38f7ccba0f3fa0228e1
TianZer <TianLin2112@outlook.com>
2019-07-15 18:38:13 +0800

    Testing

--- swjtu_code_web/settings.py
+++ swjtu_code_web/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME': 'swjtu_code_web_mainDB',
         'USER': 'root',
         'PASSWORD': '2333666',
-        'HOST': '192.168.12.108',
+        'HOST': 'localhost',#'192.168.12.108',
         'PORT': '3306'
     }
 }
